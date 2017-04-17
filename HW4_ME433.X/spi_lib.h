93d09397a7b8acbd9cb3a8edf89ea79dd790324f
harishchockalingam <harishchockalingam2017@u.northwestern.edu>
2017-04-16 23:50:30 -0500

    SPI code

---
+++ HW4_ME433.X/spi_lib.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   spi_lib.h
+ * Author: Harish
+ *
+ * Created on April 13, 2017, 7:27 PM
+ */
+
+#ifndef SPI_LIB_H
+#define	SPI_LIB_H
+
+unsigned char spi_io(unsigned char o);
+void init();
+void setVoltage(unsigned char channel, unsigned char voltage);
+unsigned char tWave(int a, int b, int c);
+unsigned char sWave(int a, int b, int c);
